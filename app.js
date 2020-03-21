253c6f5c28fa65d3adb53febe85574d4da2a2371
angrychanin <sun.chanin@gmail.com>
2020-03-21 10:38:28 +0700

    First Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require("express"),
+  app = express();
+
+// Set up
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+//Routes
+app.get("/", (req, res) => {
+  res.render("portfolio/start");
+});
+
+// Port
+let port = 3000;
+app.listen(port, () => {
+  console.log("Server Start!");
+});
